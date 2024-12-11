040747fdea9e13059b66231d107a4bc50f7ed15c
jon geiger <git@geiger.to>
2024-12-10 23:51:57 -0500

    improve tests for good_job and sidekiq web routes

--- test/integration/good_job_web_test.rb
+++ test/integration/good_job_web_test.rb
@@ -3,14 +3,18 @@ require "test_helper"
 class GoodJobWebTest < MasksTestCase
   include AuthHelper
 
-  setup { Masks.env.queue_adapter = :good_job }
+  setup do
+    Masks.env.queue_adapter = :good_job
+    Rails.application.reload_routes!
+  end
 
   test "good_job-web is available to managers" do
     log_in "manager"
 
     get "/manage/queue"
 
-    assert_equal 200, status
+    assert_equal 302, status
+    assert_redirected_to "/manage/queue/jobs?locale=en"
   end
 
   test "good_job-web redirects to login for non-managers" do
--- test/integration/sidekiq_web_test.rb
+++ test/integration/sidekiq_web_test.rb
@@ -3,7 +3,10 @@ require "test_helper"
 class SidekiqWebTest < MasksTestCase
   include AuthHelper
 
-  setup { Masks.env.queue_adapter = :sidekiq }
+  setup do
+    Masks.env.queue_adapter = :sidekiq
+    Rails.application.reload_routes!
+  end
 
   test "sidekiq-web is available to managers" do
     log_in "manager"
@@ -11,6 +14,7 @@ class SidekiqWebTest < MasksTestCase
     get "/manage/queue"
 
     assert_equal 200, status
+    assert_includes response.body, "<title>[TEST] Sidekiq</title>"
   end
 
   test "sidekiq-web redirects to login for non-managers" do
